007510a8d9b8f26c9c6a3edbb686e7eaaf2efb10
Moritz Schüßler <mail@moritzschuessler.de>
2026-04-26 00:11:55 +0200

    build(deps): add @tanstack/react-query

--- apps/auth/eslint.config.js
+++ apps/auth/eslint.config.js
@@ -1,5 +1,5 @@
 //  @ts-check
-
 import { tanstackConfig } from "@tanstack/eslint-config"
+import pluginQuery from "@tanstack/eslint-plugin-query"
 
-export default [...tanstackConfig]
+export default [...tanstackConfig, ...pluginQuery.configs["flat/recommended"]]
