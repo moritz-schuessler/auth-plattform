634a2ad362b7739825c49ea4ed261c6b6bafc738
Moritz Schüßler <mail@moritzschuessler.de>
2026-04-26 03:48:31 +0200

    feat: add pages for login and sign-up

--- apps/auth/src/routes/__root.tsx
+++ apps/auth/src/routes/__root.tsx
@@ -38,7 +38,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <head>
         <HeadContent />
       </head>
-      <body>
+      <body className="h-dvh">
         {children}
         <Scripts />
       </body>
