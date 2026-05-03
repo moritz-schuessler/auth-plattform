a51043e4a6ef76a7cacd866fc37a5eb5eedfcad9
Moritz Schüßler <mail@moritzschuessler.de>
2026-05-03 19:50:45 +0200

    feat: show a list of users in the admin panel

--- apps/auth/src/routes/_authenticated/_admin/admin.tsx
+++ apps/auth/src/routes/_authenticated/_admin/admin.tsx
@@ -1,16 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { getRequestHeaders } from "@tanstack/react-start/server"
+import { authClient } from "@/lib/auth/client/auth-client"
 
 export const Route = createFileRoute("/_authenticated/_admin/admin")({
+  loader: async () => {
+    const headers = getRequestHeaders()
+
+    return await authClient.admin.listUsers({
+      query: {},
+      fetchOptions: { headers },
+    })
+  },
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const data = Route.useRouteContext()
+  const { data } = Route.useLoaderData()
 
   return (
-    <main className="flex w-full flex-col p-4">
+    <main className="flex w-full flex-col gap-4 p-4">
       <h1 className="text-2xl">Admin</h1>
-      <p className="mt-2">You are {data.user.role}</p>
+      <ul>
+        {data?.users.map((user) => (
+          <li key={user.id}>{user.email}</li>
+        ))}
+      </ul>
     </main>
   )
 }
