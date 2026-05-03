981983943ad3ad88e95ddc8d9dc01359a843f761
Moritz Schüßler <mail@moritzschuessler.de>
2026-05-03 23:12:09 +0200

    fix: prevent client side call of getRequestHeaders

--- apps/auth/src/routes/_authenticated/_admin/admin.tsx
+++ apps/auth/src/routes/_authenticated/_admin/admin.tsx
@@ -1,27 +1,32 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { createServerFn } from "@tanstack/react-start"
 import { getRequestHeaders } from "@tanstack/react-start/server"
-import { authClient } from "@/lib/auth/client/auth-client"
+import { auth } from "@/lib/auth/server/auth"
+
+const listUsers = createServerFn({ method: "GET" }).handler(async () => {
+  const headers = getRequestHeaders()
+
+  return await auth.api.listUsers({
+    headers,
+    query: {},
+  })
+})
 
 export const Route = createFileRoute("/_authenticated/_admin/admin")({
   loader: async () => {
-    const headers = getRequestHeaders()
-
-    return await authClient.admin.listUsers({
-      query: {},
-      fetchOptions: { headers },
-    })
+    return await listUsers()
   },
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const { data } = Route.useLoaderData()
+  const { users } = Route.useLoaderData()
 
   return (
     <main className="flex w-full flex-col gap-4 p-4">
       <h1 className="text-2xl">Admin</h1>
       <ul>
-        {data?.users.map((user) => (
+        {users.map((user) => (
           <li key={user.id}>{user.email}</li>
         ))}
       </ul>
